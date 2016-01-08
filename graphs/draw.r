3b55f9f131a7388b2f00aa7603673e35e2f3e356
Redi <konieczny93@gmail.com>
2016-01-08 11:07:55 +0100

    update

--- graphs/draw.r
+++ graphs/draw.r
@@ -20,3 +20,41 @@ plot(
 pdf('cluster.pdf')
 plot(x,y)
 dev.off()
+
+
+
+
+library(fpc)
+library(cluster) 
+
+data(iris)
+fit <- kmeans(iris, 2)
+
+clusplot(iris, fit$cluster, color=TRUE, shade=TRUE, 
+  	labels=2, lines=0)
+plotcluster(iris, fit$cluster)
+
+
+library(ggvis)
+iris %>% ggvis(~Sepal.Length, ~Sepal.Width, fill = ~Species) %>% layer_points()
+
+
+library(ggplot2)
+ggplot(np_graph) + geom_point(aes(x = C1, y = C2, colour = C1 >0)) +
+  scale_colour_manual(name = 'PC1 > 0', values = setNames(c('red','green'),c(T, F))) +
+  xlab('PC1') + ylab('PC2')
+
+  plot(x,y,xlab="PC1",ylab="PC2", col = ifelse(x < 0,'red','green'), pch = 19 
+
+
+  	rsvg-convert -f pdf -o t.pdf t.svg
+
+
+
+  	\begin{figure}[h]
+    \centering
+    \textbf{Rozmieszczenie kwiatów irysów}\par\medskip
+    \includegraphics[scale=0.7]{plotsepalwidthsepallength}
+    \caption{Opracowanie własne}
+    \label{fig:plotsepalwidthsepallength}
+\end{figure}
